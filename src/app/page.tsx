b97e832151ba28ad001efcb7567e822369b6195f
Sanjid Islam Chowdhury <56227702+LassassinX@users.noreply.github.com>
2024-01-07 16:58:35 +0600

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,30 @@
 import Image from 'next/image'
 import { handlePayment } from './actions'
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 
 
 export default function Home() {
 	const router = useRouter()
+	const [loading, setLoading] = useState(false)
 
 	return (<>
 		<h1>
 			Hay you whats up man gimme monies pliss
 		</h1>
-		<button className='btn mb-20' onClick={async () => {
-			const x = await handlePayment()
-			if (x?.redirect) {
-				router.push(x.redirect)
-			}
+		<button disabled={loading} className='btn mb-20' onClick={async () => {
+			setLoading(true)
+			try {
+				const x = await handlePayment()
+				if (x?.redirect) {
+					router.push(x.redirect)
+				}
+			} catch (error) {
+				console.error(error)
+			} finally {
+				setLoading(false)
+			}	
 		}}>
 			Pay monies
 		</button>
